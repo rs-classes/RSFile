4072d698414d5f88d933074fa0f69009612458a9
Robert Stevens <robert@stevens.tk>
2015-02-02 02:44:59 +0100

    made a start on the file handle classes

--- RSFile.cpp
+++ RSFile.cpp
@@ -1,2 +1,54 @@
 #include "header/RSFile.h"
 
+RSFile::RSFile(const char* fileName, const char* mode);
+{
+  m_fileName = fileName;
+  m_isOpen = false;
+  m_mode = mode;
+  mp_fp = NULL;
+  
+  mp_fp = fopen(m_fileName, m_mode);
+  if (mp_fp != NULL)
+    m_isOpen = true;
+}
+char* RSFile::ReadLine()
+{
+}
+char* RSFile::ReadChar()
+{
+  //FILE *fp;
+  // int c;
+  
+  //fp = fopen("file.txt","r");
+  //while(1)
+  //{
+  // c = fgetc(fp);
+  //if( feof(fp) )
+  //{ 
+  //   break ;
+  // }
+  // printf("%c", c);
+  //}
+}
+
+int RSFile::Write(const char* text);
+{
+  int chars = fprintf(mp_fp, "%s",text);
+  if(chars > 0)
+    return 0;
+  return RSFILE_ERROR_WRITE;
+}
+int RSFile::Write(char text)
+{
+}
+
+RSFile::~RSFile()
+{
+  if(m_isOpen)
+    {
+      fclose(mp_fp);
+      m_isOpen = false;
+    }
+}
+
+  
---
+++ header/RSFile.h
@@ -0,0 +1,30 @@
+#ifndef RSFILE_H_
+#define RSFILE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RSFILE_ERROR_START 1000
+#define RSFILE_ERROR_WRITE (RSFILE_ERROR_START+1)
+
+class RSFile{
+ public:
+  RSFile(const char* fileName, onst char *mode);
+  char* ReadLine();
+  char* ReadChar();
+  
+  int Write(const char* text);
+  int Write(char text);
+  
+  bool IsOpen();
+  
+  ~RSFile();
+  
+ private:
+  bool m_isOpen;
+  const char* m_fileName;
+  const char* m_mode;
+  FILE* mp_fp;
+};
+
+#endif
